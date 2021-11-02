a340662aa8fb404c6b646635d59991e145ad1f57
BaekJae <kis89576@naver.com>
2021-11-02 20:39:13 +0900

    test page upload

--- express_server/routes/db.js
+++ express_server/routes/db.js
@@ -1,26 +1,38 @@
-var express = require('express');
-var router = express.Router();
-var sequelize = require('sequelize');
-export default(db_client) =>
-{
-    /* register */
-    router.post('/users/register', function (req, res, next) {
+const express = require('express');
+const sequelize = require('sequelize');
+const router = express.Router();
 
+/* register */
+router.post('/users/register', function (req, res, next) {
+
+});
+
+/* userinfo get */
+router.get('/users/userinfo', function (req, res, next) {
+    var success_response={
+        seat_no: "1SA1",
+        seat_available: 0,
+        pc_available: 0,
+        concent_available: 0,
+        preferperson_no: 0
+    };
+    var resStr = JSON.stringify(success_response);
+    res.render('test', { seat_no: success_response.seat_no,
+        seat_available: success_response.seat_available,
+        pc_available: success_response.pc_available,
+        concent_available: success_response.concent_available,
+        preferperson_no: success_response.preferperson_no
     });
+});
 
-    /* userinfo get */
-    router.get('/users/userinfo', function (req, res, next) {
+/* seats state change */
+router.post('/seats/change', function (req, res, next) {
 
-    });
+});
 
-    /* seats state change */
-    router.post('/seats/change', function (req, res, next) {
+/* seats info get */
+router.get('/seats/get', function (req, res, next) {
 
-    });
+});
+module.exports = router;
 
-    /* seats info get */
-    router.get('/seats/get', function (req, res, next) {
-
-    });
-    return router;
-}
