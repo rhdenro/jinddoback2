5ffd7d1136fca589e0605251b42d9a827358ed47
kis89576@naver.com <kis89576@naver.com>
2021-11-03 16:46:21 +0900

    Sequelize 초기 설정, Seat 정보 get, update 구현

--- express_server/routes/db.js
+++ express_server/routes/db.js
@@ -1,26 +1,52 @@
 var express = require('express');
 var router = express.Router();
 var sequelize = require('sequelize');
-export default(db_client) =>
-{
-    /* register */
-    router.post('/users/register', function (req, res, next) {
+var mysql = require('mysql2');
 
+/* register */
+router.post('/users/register', function (req, res, next) {
+
+});
+
+/* userinfo get */
+router.get('/users/userinfo', function (req, res, next) {
+
+});
+
+/* seats state change */
+router.post('/seats/change', function (req, res, next) {
+    var connection = mysql.createConnection({
+        host: process.env.MYSQL_HOST,
+        user: process.env.MYSQL_USER,
+        password: process.env.MYSQL_PASSWORD,
+        database: process.env.MYSQL_DATABASES
     });
+    connection.connect();
+    var sql = 'UPDATE seats SET seat_available=0 WHERE seat_code=?'
+    connection.query(sql,[req.body.seat_code], function(err, result, fields){
+        if(err) throw(err)
+        console.log('update successful');
+    })
+    connection.end();
+    res.json('successful');
+});
 
-    /* userinfo get */
-    router.get('/users/userinfo', function (req, res, next) {
-
+/* seats info get */
+router.get('/seats/get', function (req, res, next) {
+    console.log(process.env.MYSQL_TABLES);
+    var connection = mysql.createConnection({
+        host: process.env.MYSQL_HOST,
+        user: process.env.MYSQL_USER,
+        password: process.env.MYSQL_PASSWORD,
+        database: process.env.MYSQL_DATABASES
     });
+    connection.connect();
+    connection.query('SELECT * FROM seats WHERE seat_code="1JA1"', function(err, result, fields){
+        if(err) throw err;
+        console.log(result);
+        console.log('load successful');
+    })
+    connection.end();
+});
 
-    /* seats state change */
-    router.post('/seats/change', function (req, res, next) {
-
-    });
-
-    /* seats info get */
-    router.get('/seats/get', function (req, res, next) {
-
-    });
-    return router;
-}
+module.exports = router;
