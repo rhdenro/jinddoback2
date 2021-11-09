abbff6a6f1d2b8cde3b97f645405739844735695
유진선 <75780733+rhdenro@users.noreply.github.com>
2021-11-09 17:36:19 +0900

    콘센트 사용 가능 좌석 추출 부분 추가 콘센트를 사용 한다고 입력이 들어오면 콘센트 좌석은 가장자리가 존재 하기 때문에 가장자리를 원한다면 좌석 데이터프레임에서 가장자리가 아닌 좌석은 제거 상관이 없다면 포함해서 추출

--- proto _type.py
+++ proto _type.py
@@ -1,5 +1,5 @@
 import pandas as pd
-result=[]
+result = []
 df = pd.read_excel(r'C:\Users\user\Desktop\data.xlsx')
 test_df = pd.read_excel(r'C:\Users\user\Desktop\test.xlsx')
 backup_df = df
@@ -7,7 +7,9 @@ print(" 콘센트 사용 가능 유무 (1:가능 , 2: 불가능)")
 con = input()
 print(" pc 사용 가능 유무 (1:가능 , 2: 불가능)")
 com = input()
-level_2_flag=0
+print(" 가장자리 여부 (1:YES , 2: no matter)")
+edge=input()
+level_2_flag = 0
 flag = df['사용여부'] == 1
 df = df[flag]
 if (com == '1') :
@@ -21,21 +23,26 @@ elif (con == '1') :
     df = df[flag]
     df['점수'] = 0
 
-
+# pc사용 가능 좌석 처리
 if level_2_flag == 1:
     flag = test_df['별점'] == 5
     tmp_test = test_df[flag]
     for i in tmp_test['좌석 코드']:
         flag = df['좌석 번호'] == i
-        if df[flag].empty :continue
+        if df[flag].empty: continue
         result.append(df[flag].iloc[0])
         result[-1]['점수'] = 1
         idx_df = df[flag].index
         df = df.drop(idx_df)
-    print(result)
     while(len(result) <= 15):
         if df.empty: break
         result.append(df.iloc[0])
         df = df.drop(df.index[0])
 
-print(result)
+# 콘센트 사용 가능 좌석 처리
+elif(level_2_flag == 2):
+    flag = test_df['별점'] == 5
+    tmp_test = test_df[flag]
+    if edge == '1':
+        df = df[df['가장자리'] == 1]
+print(df)
