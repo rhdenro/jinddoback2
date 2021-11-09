a20cadcf6a9ed29f9d8006cac5d5a04bb181f19d
kis89576@naver.com <kis89576@naver.com>
2021-11-09 16:43:22 +0900

    logout 구현, 조건부 메인 페이지 모델링 구현

--- express_server/routes/index.js
+++ express_server/routes/index.js
@@ -16,4 +16,14 @@ router.get('/login', function(req,res,next){
   res.render('login', { title: 'Library seat Recommendation System', subtitle: 'Login', session: req.session})
 });
 
+/* Logout function */
+router.get('/logout', function(req,res,next){
+  delete req.session.userid;
+  delete req.session.name;
+  delete req.session.isLogined;
+  req.session.save(function(){
+    res.send('<script> alert("로그아웃 완료"); location.href="/" </script>')
+  })
+})
+
 module.exports = router;
