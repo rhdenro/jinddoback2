468d1cca828a1313ead8c89c5fc5efd2ec92d7fe
유진선 <75780733+rhdenro@users.noreply.github.com>
2021-11-11 19:34:05 +0900

    우대 인원 처리 부분 2인은 컴퓨터좌석 3인은 일반 좌석 3인 우대 일반 좌석 처리 추가 인덱스를 이용 시작 좌석 인덱스이면서 뒤에 2개의 연속된 인덱스의 자리가 있다면 3좌석을 데이터 프레임으로 묶은뒤 json객체로 리스트에 삽입

--- proto _type.py
+++ proto _type.py
@@ -14,15 +14,36 @@ if (fre == 1):
     p_num = int(input())
     flag = df['우대인원'] == p_num
     df = df[flag]
-    print(df)
     if p_num==2:
         count=df.shape[0]
         for i in range(count-1):
+            print(i)
             if int(df['좌석 번호'].iloc[i][3])%2==1:
                 if int(df['좌석 번호'].iloc[i+1][3])%2==0:
                     temp=pd.concat([df.iloc[i],df.iloc[i+1]], axis=1)
                     result.append(temp.to_json)
                     i+=1
+    elif p_num==3:
+        start=[192,195,198,201,204,207,210,213,216,219,222,225,225,228,231,234]
+        i=0
+        count = df.shape[0]
+        while i<= (count - 3):
+            print(df.index[i])
+            if df.index[i] in start:
+                if df.index[i]-df.index[i+1]==-1 and df.index[i+1]-df.index[i+2]==-1:
+                    temp = pd.concat([df.iloc[i], df.iloc[i + 1] , df.iloc[i+2]], axis=1)
+                    i = i + 3
+                    result.append(temp.to_json)
+                else :i = i+1
+            else:
+                i=i+1
+        print(result)
+
+
+
+
+
+
 
 else:
     print(" 콘센트 사용 가능 유무 (1:가능 , 2: 불가능)")
