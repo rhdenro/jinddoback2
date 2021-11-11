2685dcf343e5e4d1896fbe512f306570fc993e37
kis89576@naver.com <kis89576@naver.com>
2021-11-11 17:35:48 +0900

    Recommendation 페이지 링크 추가

--- express_server/routes/index.js
+++ express_server/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Library seat Recommendation System', session: req.session});
+  res.render('myinfo', { title: 'Library seat Recommendation System', session: req.session});
 });
 
 /* Register Page */
@@ -16,13 +16,18 @@ router.get('/login', function(req,res,next){
   res.render('login', { title: 'Library seat Recommendation System', subtitle: 'Login', session: req.session})
 });
 
+/* recommendation page */
+router.get('/recommendation', function(req,res,next){
+  res.render('recommendation', { title: 'Library seat Recommendation System', subtitle: 'recommendation', session: req.session})
+});
+
 /* Logout function */
 router.get('/logout', function(req,res,next){
   delete req.session.userid;
   delete req.session.name;
   delete req.session.isLogined;
   req.session.save(function(){
-    res.send('<script> alert("로그아웃 완료"); location.href="/" </script>')
+    res.send('<script> alert("로그아웃 완료"); location.href="/" </script>');
   })
 })
 
