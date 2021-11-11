3b9084b8633ecf2a334a6b943832f56bb006c73b
유진선 <75780733+rhdenro@users.noreply.github.com>
2021-11-11 17:34:02 +0900

    우대 인원 처리 부분 우대인원을 가장 우선적으로 처리하게 코드 변경 좌석이 2인, 3인만 데이터상 존재하기 때문에 2 or 3의 입력이 들어오면 값에 맞게 따로 구현 예정

--- proto _type.py
+++ proto _type.py
@@ -3,58 +3,68 @@ result = []
 df = pd.read_excel(r'C:\Users\user\Desktop\data.xlsx')
 test_df = pd.read_excel(r'C:\Users\user\Desktop\test.xlsx')
 backup_df = df
-print(" 콘센트 사용 가능 유무 (1:가능 , 2: 불가능)")
-con = input()
-print(" pc 사용 가능 유무 (1:가능 , 2: 불가능)")
-com = input()
-print(" 가장자리 여부 (1:YES , 2: no matter)")
-edge=input()
-level_2_flag = 0
-flag = df['사용여부'] == 1
-df = df[flag]
-if (com == '1') :
-    level_2_flag = 1
-    flag = df['pc유무'] == 1
+print("우대인원 사용 가능 (1: 사용, 2. 미사용")
+fre = input()
+if (fre==0):
+    print("좌석 분류: pc석(2인), 일반석(3인)")
+    print("1: pc, 2: 일반")
+    p_num = input()
+    flag = df['우대인원'] == p_num
     df = df[flag]
-    df['점수'] = 0
-elif (con == '1') :
-    level_2_flag = 2
-    flag = df['콘센트유무'] == 1
+    print(df)
+else:
+    print(" 콘센트 사용 가능 유무 (1:가능 , 2: 불가능)")
+    con = input()
+    print(" pc 사용 가능 유무 (1:가능 , 2: 불가능)")
+    com = input()
+    print(" 가장자리 여부 (1:YES , 2: no matter)")
+    edge = input()
+    level_2_flag = 0
+    flag = df['사용여부'] == 1
     df = df[flag]
-    df['점수'] = 0
+    if (com == '1'):
+        level_2_flag = 1
+        flag = df['pc유무'] == 1
+        df = df[flag]
+        df['점수'] = 0
+    elif (con == '1'):
+        level_2_flag = 2
+        flag = df['콘센트유무'] == 1
+        df = df[flag]
+        df['점수'] = 0
 
-# pc사용 가능 좌석 처리
-if level_2_flag == 1:
-    flag = test_df['별점'] == 5
-    tmp_test = test_df[flag]
-    for i in tmp_test['좌석 코드']:
-        flag = df['좌석 번호'] == i
-        if df[flag].empty: continue
-        result.append(df[flag].iloc[0])
-        result[-1]['점수'] = 1
-        idx_df = df[flag].index
-        df = df.drop(idx_df)
-    while(len(result) <= 15):
-        if df.empty: break
-        result.append(df.iloc[0])
-        df = df.drop(df.index[0])
+    # pc사용 가능 좌석 처리
+    if level_2_flag == 1:
+        flag = test_df['별점'] == 5
+        tmp_test = test_df[flag]
+        for i in tmp_test['좌석 코드']:
+            flag = df['좌석 번호'] == i
+            if df[flag].empty: continue
+            result.append(df[flag].iloc[0])
+            result[-1]['점수'] = 1
+            idx_df = df[flag].index
+            df = df.drop(idx_df)
+        while (len(result) <= 15):
+            if df.empty: break
+            result.append(df.iloc[0])
+            df = df.drop(df.index[0])
 
-# 콘센트 사용 가능 좌석 처리
-elif(level_2_flag == 2):
-    flag = test_df['별점'] == 5
-    tmp_test = test_df[flag]
-    if edge == '1':
-        df = df[df['가장자리'] == 1]
-    for i in tmp_test['좌석 코드']:
-        flag = df['좌석 번호'] == i
-        if df[flag].empty:
-            continue
-        result.append(df[flag].iloc[0])
-        result[-1]['점수'] = 1
-        idx_df = df[flag].index
-        df = df.drop(idx_df)
-    while (len(result) <= 15):
-        if df.empty: break
-        result.append(df.iloc[0])
-        df = df.drop(df.index[0])
-print(result)
+    # 콘센트 사용 가능 좌석 처리
+    elif (level_2_flag == 2):
+        flag = test_df['별점'] == 5
+        tmp_test = test_df[flag]
+        if edge == '1':
+            df = df[df['가장자리'] == 1]
+        for i in tmp_test['좌석 코드']:
+            flag = df['좌석 번호'] == i
+            if df[flag].empty:
+                continue
+            result.append(df[flag].iloc[0])
+            result[-1]['점수'] = 1
+            idx_df = df[flag].index
+            df = df.drop(idx_df)
+        while (len(result) <= 15):
+            if df.empty: break
+            result.append(df.iloc[0])
+            df = df.drop(df.index[0])
+    print(result)
