b012ea7d91c5c2145666976e336356d47c8c694b
유진선 <75780733+rhdenro@users.noreply.github.com>
2021-11-23 15:10:55 +0900

    2인 우대 좌석 db와 변수, 키값 통일시키고 결과값을 일반 2차원list로 httpresponse로 변경

--- django_server/recommendation/views.py
+++ django_server/recommendation/views.py
@@ -5,20 +5,33 @@ from django.db import connection
 def recommend(request):
     try:
         import pandas as pd
+        import json
         cursor = connection.cursor()
 
-        strSql = "SELECT *  FROM seats where seat_available = 0"
+        strSql = "SELECT *  FROM seats where seat_available = 1"
         cursor.execute(strSql)
         seats = cursor.fetchall()
         connection.commit()
         connection.close()
         df = pd.DataFrame(seats, columns=['seat_available', 'pc_available', 'concent_available', 'seat_code', 'preferences', 'edge_seat'])
-        js = df.to_json(orient='columns')
+        result = []
+        temp=[]
+        fre = 1
+        p_num = 2
+        if (fre == 1):
+            flag = df['preferences'] == p_num
+            df = df[flag]
+            if p_num == 2:
+                count = df.shape[0]
+                for i in range(count - 1):
+                    if int(df['seat_code'].iloc[i][3]) % 2 == 1:
+                        if int(df['seat_code'].iloc[i + 1][3]) % 2 == 0:
+                            result.append([df['seat_code'].iloc[i], df['seat_code'].iloc[i + 1]])
+                            i += 1
 
 
     except:
         connection.rollback()
         print("Failed selecting in BookListView")
 
-    return HttpResponse(js)
-
+    return HttpResponse(result)
