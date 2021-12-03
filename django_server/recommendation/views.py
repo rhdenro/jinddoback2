7cd1ef6701d37cdc1807894fe1acf649864780fb
유진선 <75780733+rhdenro@users.noreply.github.com>
2021-12-03 21:17:42 +0900

    동일 좌석 -> 시간 차이 -> 변경없음 판별 -> 일반 변경순으로 코드 순서 교체 및 변경없음 판별 기능 추가

--- django_server/recommendation/views.py
+++ django_server/recommendation/views.py
@@ -230,9 +230,16 @@ def reservation(request):
     min_date=0
     flag_date=0
     for i in temp:
+        if i[2] == seat_code:
+            temp_count = i[3] + 1
+            strSql = 'UPDATE preference_table SET  count =(%s), score=(%s),density=(%s) WHERE reservation_user=(%s) and seat_code=(%s)'
+            cursor.execute(strSql, (temp_count, score, density, userid, seat_code,))
+            connection.commit()
+            return HttpResponse('동일')
+
+    for i in temp:
         if i[4] <min_date:
             min_date=i[4]
-
     if date - min_date > 21:
         strSql = 'DELETE FROM preference_table WHERE reservation_user=(%s) and seat_code=(%s)'
         cursor.execute(strSql, (userid, i[2],))
@@ -241,21 +248,25 @@ def reservation(request):
         cursor.execute(strSql, (userid, seat_code, 1, date, score, density,))
         connection.commit()
         flag_date=1
+        return HttpResponse("시간 차이")
+
     if flag_date !=1:
+        min_score=10
         for i in temp:
-            if i[2] == seat_code:
-                temp_count=i[3]+1
-                strSql='UPDATE preference_table SET  count =(%s), score=(%s),density=(%s) WHERE reservation_user=(%s) and seat_code=(%s)'
-                cursor.execute(strSql, (temp_count,score,density,userid,seat_code,))
-                connection.commit()
-        for i in temp:
-            if i[5] < score:
-                strSql='DELETE FROM preference_table WHERE reservation_user=(%s) and seat_code=(%s)'
-                cursor.execute(strSql, (userid, i[2] ,))
-                connection.commit()
-                strSql = 'INSERT INTO preference_table(reservation_user, seat_code , count ,date,score,density) VALUES ((%s),(%s),(%s),(%s),(%s),(%s))'
-                cursor.execute(strSql, (userid, seat_code , 1 ,date ,score ,density, ))
-                connection.commit()
+            if i[5]<min_score:
+                min_score = i[5]
+        if score<min_score:
+            return HttpResponse('변경 없음')
+        else:
+            temp.sort(key=lambda x: x[4])
+            for i in temp:
+                if i[5] <= score:
+                    strSql='DELETE FROM preference_table WHERE reservation_user=(%s) and seat_code=(%s)'
+                    cursor.execute(strSql, (userid, i[2] ,))
+                    connection.commit()
+                    strSql = 'INSERT INTO preference_table(reservation_user, seat_code , count ,date,score,density) VALUES ((%s),(%s),(%s),(%s),(%s),(%s))'
+                    cursor.execute(strSql, (userid, seat_code , 1 ,date ,score ,density, ))
+                    connection.commit()
     strSql = "SELECT * FROM preference_table where reservation_user = (%s)"
     cursor.execute(strSql, (userid,))
     connection.commit()
