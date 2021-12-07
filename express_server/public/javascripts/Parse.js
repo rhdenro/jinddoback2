eb255ddddc775b1cc598faa3ebb23041f9b2b104
kis89576@naver.com <kis89576@naver.com>
2021-12-07 22:38:06 +0900

    좌석코드 파싱 - Django 처리 결과에 따른 파싱 하위 Row 처리하는 서브함수

--- express_server/public/javascripts/Parse.js
+++ express_server/public/javascripts/Parse.js
@@ -1,12 +1,24 @@
+//우대좌석 미사용자 처리
 function repeatNonPrefer(Array){
     return new Promise(async function (resolve, reject){
-        const promises = Array.map((row) => parse(row.seat_code));
+        const promises = Array.map((row) => parseNonPrefer(row));
         await Promise.all(promises)
             .then(responses => {
                 resolve(responses);
             })
     })
 }
+//하위 row 처리
+function parseNonPrefer(row){
+    return new Promise(async function(resolve,reject){
+        const promises = row.map((subRow) => parseSingle(row));
+        await Promise.all(promises)
+            .then(responses => {
+                resolve(responses);
+            })
+    })
+}
+//최종 row 처리
 
 function repeatDefault(Array){
 
