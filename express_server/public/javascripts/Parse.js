58e16a156e3d52e548c7eb98dfdf98433d290a1b
kis89576@naver.com <kis89576@naver.com>
2021-12-07 23:25:12 +0900

    좌석코드 파싱 - Django 처리 결과에 따른 파싱 결과값 define

--- express_server/public/javascripts/Parse.js
+++ express_server/public/javascripts/Parse.js
@@ -11,16 +11,28 @@ function repeatNonPrefer(Array){
 //하위 row 처리
 function parseNonPrefer(row){
     return new Promise(async function(resolve,reject){
-        const promises = row.map((subRow) => parseSingle(row));
+        const promises = row.map((subRow) => parseCode(subRow);
         await Promise.all(promises)
-            .then(responses => {
-                resolve(responses);
+            .then(result => {
+                resolve(result);
             })
     })
 }
+
+function parseCode(subRow){
+    return new Promise(function(resolve,reject){
+        let result = new Object();
+        let seat_code = subRow[0];
+        result.density = subRow[1];
+        result.floor = "";
+        result.form  = "";
+        result.sector = "";
+        result.sectorNum = "";
+    })
+}
 //최종 row 처리
-
 function repeatDefault(Array){
-
+    return new Promise(function(resolve,reject){
+    })
 }
 module.exports = repeatNonPrefer(), repeatDefault();
