1c867f7341c332fab4784d7fc952328285247ba0
kis89576@naver.com <kis89576@naver.com>
2021-12-07 23:47:09 +0900

    좌석코드 파싱 - Django 처리 결과에 따른 파싱 - 우대좌석 처리 함수 완료

--- express_server/public/javascripts/Parse.js
+++ express_server/public/javascripts/Parse.js
@@ -86,7 +86,67 @@ function repeatPrefer(Array){
 }
 
 function parsePrefer(row){
-
+    let result = new Object();
+    let seat_code = row[0];
+    result.seatCode = row;
+    result.density = 'X';
+    result.floor = "";
+    result.form  = "";
+    result.sector = "";
+    result.seatNum = "";
+    //층수 추출
+    switch(seat_code[0]){
+        case '1':
+            result.floor = "1층";
+            break;
+        case '2':
+            result.floor = "2층";
+            break;
+        case '3':
+            result.floor = "3층";
+            break;
+        case '4':
+            result.floor = "4층";
+            break;
+    }
+    //형태 분류
+    switch(seat_code[1]){
+        case 'S':
+            result.form = "스마트";
+            break;
+        case 'J':
+            result.form = "조망형";
+            break;
+        case 'P':
+            result.form = "PC룸";
+            break;
+        case 'N':
+            result.form = "노트북 전용";
+            break;
+    }
+    //섹터 분류
+    switch(seat_code[2]){
+        case 'A':
+            result.sector = "A섹터";
+            break;
+        case 'B':
+            result.sector = "B섹터";
+            break;
+        case 'C':
+            result.sector = "C섹터";
+            break;
+        case 'D':
+            result.sector = "D섹터";
+            break;
+    }
+    //좌석 번호
+    if(seat_code[4]){
+        result.seatNum = ((seat_code[3]-'0')*10) + (seat_code[4]-'0');
+    }
+    else{
+        result.seatNum = seat_code[3]-'0';
+    }
+    return result;
 }
 
-module.exports = repeatNonPrefer(), repeatDefault();
+module.exports = repeatNonPrefer(), repeatPrefer();
