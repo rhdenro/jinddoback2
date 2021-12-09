5e295f818466e0c1036f2009df523bfdb93cb19c
jaejin28 <kmig1164@naver.com>
2021-12-10 02:57:54 +0900

    별점 데이터 기능 구현

--- seat/src/Rating.js
+++ seat/src/Rating.js
@@ -2,3 +2,30 @@ import React, { useState, useEffect } from 'react';
 import { FaStar } from 'react-icons/fa';
 import styled from 'styled-components';
 
+const ARRAY = [0, 1, 2, 3, 4];
+
+function Rating() {
+  const [clicked, setClicked] = useState([false, false, false, false, false]);
+
+  const handleStarClick = index => {
+    let clickStates = [...clicked];
+    for (let i = 0; i < 5; i++) {
+      clickStates[i] = i <= index ? true : false;
+    }
+    setClicked(clickStates);
+  };
+
+  useEffect(() => {
+    sendReview();
+  }, [clicked]); //컨디마 컨디업
+
+  const sendReview = () => {
+    let score = clicked.filter(Boolean).length;
+
+  };
+
+  
+}
+
+export default Rating;
+
