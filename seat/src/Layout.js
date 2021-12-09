94dc95531826ae026749acdfc1549a3bb93b1403
jaejin28 <kmig1164@naver.com>
2021-12-10 02:36:05 +0900

    메인페이지 Layout import

---
+++ seat/src/Layout.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./Layout.css";
+import pagehome from './images/pagehome.jpeg';
+import Kategorie from './Kategorie';
+import everyseat from './images/everyseat.jpg';
+import recomendation from './images/recomendation.png';
+import time from './images/time.png';
+import user from './images/user.png';
+import Header from './Header.js';
+
