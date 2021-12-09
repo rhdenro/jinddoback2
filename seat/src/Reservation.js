ebb055b5efead48ee002625d9229b04df95c4579
jaejin28 <kmig1164@naver.com>
2021-12-10 03:12:57 +0900

    예약하기 페이지 출력 구현

--- seat/src/Reservation.js
+++ seat/src/Reservation.js
@@ -8,5 +8,24 @@ export default function Reservation(){
        
         setShowPopup(event.target.value)
     };
-   
+    return (
+        
+        <div className="Reservation">
+            <h1> 좌석 정보 :  </h1>
+            <button className="open" onClick={togglePopup} value = 'false'> 
+            예약하기</button>
+
+            {showPopup ? (
+                <div className="popup">
+                    <div className="popup_inner">
+                        <br/>
+                        <h2> 예약 완료!! </h2>
+                        <button className="close" onClick={togglePopup}>
+                         메시지 닫기
+                        </button>
+                    </div>
+                    </div>
+            ): null}
+        </div>
+    );
 };
