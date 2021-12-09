95f0abcd51de93496ade00fb6a1305e3b1ecbbca
jaejin28 <kmig1164@naver.com>
2021-12-10 02:42:21 +0900

    나의 이용기록 페이지 import

---
+++ seat/src/Mypage.js
@@ -0,0 +1,15 @@
+import React, {useState} from 'react';
+import './Mypage.css';
+import Header from './Header.js';
+import Userinfo from './Userinfo.js';
+import './Mypage.css';
+import { styled } from '@mui/material/styles';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import axios from 'axios';
+
