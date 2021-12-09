dbe71a712d123262681d1986b96abd13fb006120
jaejin28 <kmig1164@naver.com>
2021-12-10 03:12:15 +0900

    예약 종료 팝업 메시지 기능 함수 구현

--- seat/src/Reservation.js
+++ seat/src/Reservation.js
@@ -1,3 +1,12 @@
 import React, {useState} from 'react';
 import './App.css';
 import './Reservation.css';
+
+export default function Reservation(){
+    const [showPopup, setShowPopup ] = useState(false);
+    const togglePopup = (event) => {
+       
+        setShowPopup(event.target.value)
+    };
+   
+};
