3486296a4f30c719bbb8e1d7c5577cf0792d90a7
jaejin28 <kmig1164@naver.com>
2021-12-10 02:10:52 +0900

    연장하기 팝업창 function

--- seat/src/Extend.js
+++ seat/src/Extend.js
@@ -11,5 +11,22 @@ function Extend(){
     const [showPopupExit, setShowPopupExit] = useState(false);
     const [showExtend, setShowExtend] = useState(true);
     const [showExit, setShowExit] = useState(true);
-    
+    //const [extend, setExtend] = useState();
+    //const [exit, setExit] = useState();
+
+    const togglePopupextend = (event) => {
+        /*axios.post(process.env.REACT_APP_SERVER_URL+":"+process.env.REACT_APP_SERVER_PORT+'/db//users/getMyPrefer',{
+        userid: sessionStorage.getItem('userId'),
+        },{'Access-Contorl-Allow-Credentials': "true",
+        withCredentials: true})
+        .then(result => {
+        console.log(result);
+        setExtend(result);
+        }) */
+        a=1;
+        setShowExit(!(event.target.value));
+        setShowPopup(event.target.value);
+
+    };
+   
 } export default Extend;
