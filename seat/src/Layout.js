949ae66d29c14992aa56fa8fa06291bfa6eb0c83
jaejin28 <kmig1164@naver.com>
2021-12-10 02:37:26 +0900

    메인페이지 4가지 기능 중 2가지 기능 구현(첫번째 row)

--- seat/src/Layout.js
+++ seat/src/Layout.js
@@ -8,3 +8,23 @@ import time from './images/time.png';
 import user from './images/user.png';
 import Header from './Header.js';
 
+function Layout(){ 
+    return(
+        <div className="Layout">
+            <Header/>
+            <div className="Layoutcontainer">
+                <img className="Layoutimage"  src={pagehome} alt="pagehome"/>
+
+                <div className="Layoutrow">
+                    <Kategorie info="전체좌석보기" image={everyseat}/>
+                    <Kategorie info="추천받기" image={recomendation}/>
+                </div>
+                
+            </div>
+
+        </div>
+
+    );
+
+}
+export default Layout;
