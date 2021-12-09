f54061f80899084ac08d479f7b91c628159734d6
kis89576@naver.com <kis89576@naver.com>
2021-12-09 23:28:31 +0900

    예약 기록 좌석 파서 함수 완성

--- express_server/public/javascripts/Parse.js
+++ express_server/public/javascripts/Parse.js
@@ -12,7 +12,6 @@ const repeatNonPrefer = (Array) => {
 //하위 row 처리
 function parseNonPrefer(row){
     let result = new Object();
-
     let seat_code = row[0];
     result.seatCode = seat_code;
     result.density = row[1];
@@ -150,7 +149,69 @@ function parsePrefer(row){
     return result;
 }
 
+//예약 기록 파서
+const repeatPreferLog = (Array) => {
+    return new Promise(async function(resolve, reject){
+        const promises = Array.map((row) => parsePreferLog(row));
+        await Promise.all(promises)
+            .then(responses => {
+                resolve(responses);
+            })
+    })
+}
+
+function parsePreferLog(row){
+    return new Promise(function(resolve,reject){
+        let result = new Object();
+        let seat_code = row.seat_code;
+        result.shape = "";
+        result.floor = "";
+        result.seatNo = "";
+        //층수 추출
+        switch(seat_code[0]){
+            case '1':
+                result.floor = "1층";
+                break;
+            case '2':
+                result.floor = "2층";
+                break;
+            case '3':
+                result.floor = "3층";
+                break;
+            case '4':
+                result.floor = "4층";
+                break;
+        }
+        //형태 분류
+        switch(seat_code[1]){
+            case 'S':
+                result.form = "스마트";
+                break;
+            case 'J':
+                result.form = "조망형";
+                break;
+            case 'P':
+                result.form = "PC룸";
+                break;
+            case 'N':
+                result.form = "노트북 전용";
+                break;
+        }
+        //좌석 번호
+        if(seat_code[4]){
+            result.seatNum = ((seat_code[3]-'0')*10) + (seat_code[4]-'0');
+        }
+        else{
+            result.seatNum = seat_code[3]-'0';
+        }
+        result.date = row.date;
+        result.score = row.score;
+
+    })
+}
+
 module.exports = {
     repeatPrefer,
-    repeatNonPrefer
+    repeatNonPrefer,
+    repeatPreferLog,
 }
