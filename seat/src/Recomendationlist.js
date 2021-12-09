7caebeec6fc56bc143c47019291e293765188285
jaejin28 <kmig1164@naver.com>
2021-12-10 03:01:34 +0900

    추천 리스트 import

---
+++ seat/src/Recomendationlist.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import { styled } from '@mui/material/styles';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import Header from './Header.js';
+import Selectbox from './Selectbox.js';
+import Switchbox from './Switchbox.js';
+import './Recomendationlist.css';
+import Reservation from './Reservation.js';
+import axios from 'axios';
+
+
