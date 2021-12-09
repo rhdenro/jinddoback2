8dbdff89cfcb87ffc17bb52b6a2650ec2fdf1f0a
jaejin28 <kmig1164@naver.com>
2021-12-10 02:33:06 +0900

    메인페이지 4가지 기능 카테고리

--- seat/src/Kategorie.js
+++ seat/src/Kategorie.js
@@ -5,6 +5,22 @@ import './Kategorie.css';
 
 
 function Kategorie({ info, image }){
+    const clickMe = () => {
+        if (info == "전체좌석보기"){
+            document.location.href='/Allseat';
+        }
+        else if(info == "추천받기"){
+            document.location.href='/Recomendationlist';
+        }
+        else if(info == "연장하기 및 종료하기"){
+            document.location.href='/Extend';
+        }
+        else if (info == "나의 이용기록"){
+            document.location.href='/Mypage';
+        }
+ 
+    }
+
    
 }
 export default Kategorie;
