ccf4177e977e3a95b29c2675b7cc02879f1aa1d9
jaejin28 <kmig1164@naver.com>
2021-12-10 03:13:54 +0900

    우대인원 selectbox 기능 구현 import

---
+++ seat/src/Selectbox.js
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import {useState} from 'react';
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
+import './Selectbox.css';
+
+
