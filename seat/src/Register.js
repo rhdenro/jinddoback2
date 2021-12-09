67b13499f90f2f3168b5fff95b556ddc956f6861
jaejin28 <kmig1164@naver.com>
2021-12-10 03:10:10 +0900

    회원가입 페이지 수정

--- seat/src/Register.js
+++ seat/src/Register.js
@@ -1,4 +1,5 @@
 import './App.css';
+import './Register.css';
 import React, {useState} from 'react';
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom';
@@ -43,20 +44,25 @@ function Register() {
         <div className="Register">
             <form>
                 <h1>회원가입</h1>
+                <br/>
                 <div>
                     <input type="text" name="name" placeholder="이름" value={name} onChange={onNameHandler} className="registerInput"/>
                 </div>
-
+               
                 <div>
                     <input type="text" name="student_no" placeholder="학번" value={studentNo} onChange={onStudentNoHandler} className="registerInput"/>
                 </div>
+             
                 <div>
                     <input type="password" name="password" placeholder="비밀번호" value={password} onChange={onPasswordHandler} className="registerInput"/>
                 </div>
+              
                 <div>
                     <input type="password" name="confirmPassword" placeholder="비밀번호 확인" value={confirmPassword} onChange={onConfirmPasswordHandler} className="registerInput"/>
                 </div>
+            
                 <div>
+                    <br/>
                     <button onClick={onSubmit}> 계정 생성하기 </button>
                 </div>
             </form>
